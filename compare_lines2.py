3119e0067e42b71011481463abdb0f084e971f11
Hui Peng Hu <woohp135@gmail.com>
2012-10-23 02:23:29 -0700

    cleanup first version

--- compare_lines2.py
+++ compare_lines2.py
@@ -1,9 +1,8 @@
 import sys
 import os
+import shutil
 import numpy as np
 import cv2
-from collections import defaultdict
-from subprocess import call
 from sklearn.cluster import DBSCAN
 
 
@@ -11,60 +10,44 @@ def main(args):
     srcFolder = "/Users/huipeng/EO990RW8/"
     linesFile = open("/Users/huipeng/EO990RW8/lines_extract.txt", 'r')
 
-    maxSizeX = 2880
-    maxSizeY = 3520
-
-    headerLine = linesFile.readline()
-    templates = []
-    templateMatches = []
-    allfeatures = []
-    allfilenames = []
-    while len(headerLine) > 0:
-        filename = headerLine.split()[0]
-        print filename
+    allFeatures = []
+    allFilenames = []
+    filename = linesFile.readline()
+    while len(filename) > 0:
         dataStr = linesFile.readline()
         features = np.fromstring(dataStr, dtype=int, sep=' ')
         features = features[:len(features)//2]
 
-#        bestScore = 0
-#        bestIndex = -1
-#        for i, template in enumerate(templates):
-#            score = np.dot(features, template)
-#            if score > bestScore:
-#                bestScore = score
-#                bestIndex = i
-#            bestMatch = max(bestScore, score)
-#
-#        print 'bestScore', bestScore, bestIndex 
-#        if bestScore > 100:
-#            templateMatches[bestIndex].append(filename)
-#        else:
-#            templates.append(features)
-#            templateMatches.append([filename])
+        allFeatures.append(features)
+        allFilenames.append(filename.strip())
 
-        allfilenames.append(filename)
-        allfeatures.append(features)
+        filename = linesFile.readline()
+    print 'finished reading all filenames. clustering...'
 
-        headerLine = linesFile.readline()
 
-    dbscan = DBSCAN(eps=1200, min_samples=2)
-    dbscan.fit(np.array(allfeatures))
+    dbscan = DBSCAN(eps=1100, min_samples=2, random_state=np.random.RandomState(0))
+    dbscan.fit(np.atleast_2d(allFeatures))
+    print 'num clusters', len(set(dbscan.labels_))
 
-    call(['rm', '-rf', '/Users/huipeng/groups/'])
-    call(['mkdir', '/Users/huipeng/groups/'])
-    for label, filename in zip(dbscan.labels_, allfilenames):
-        call(['mkdir', '/Users/huipeng/groups/' + str(label)])
-        originalImage = cv2.imread('/Users/huipeng/EO990RW8/' + filename, 0)
+
+    shutil.rmtree('/Users/huipeng/groups/')
+    os.mkdir('/Users/huipeng/groups/')
+    for label, filename in zip(dbscan.labels_, allFilenames):
+        label = str(int(label))
+
+        groupFolder = os.path.join('/Users/huipeng/groups/', label)
+        if not os.path.isdir(groupFolder):
+            os.mkdir(groupFolder)
+
+        originalImage = cv2.imread(os.path.join(srcFolder, filename), 0)
         height, width = originalImage.shape
         resizedImage = cv2.resize(originalImage, (width/4, height/4))
-        cv2.imwrite('/Users/huipeng/groups/' + str(label) + '/' + filename + '.png', resizedImage)
 
+        newFilename = os.path.join(groupFolder, filename + '.png')
+        cv2.imwrite(newFilename, resizedImage)
 
-    print 'dbscan num clusters', zip(dbscan.labels_, allfilenames)
+    print 'finished'
 
-#    for group in templateMatches:
-#        print len(group), group
-#    print len(templateMatches)
 
 
 if __name__ == '__main__':
